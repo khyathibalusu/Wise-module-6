97330b797e2fa06ca10494e00dd025f5203dba73
khyathi balusu <khyathi.balusu@gmail.com>
2015-07-09 12:21:43 +0530

    pojo class
    login bean and plain old java object

---
+++ Bug tracking system/khyathi_b-bug-tracking-system-bd94cfe26f5b/Loginbean.java
@@ -0,0 +1,19 @@
+ckage com.bts.beans;
+
+public class LoginBean {
+private String uid;
+private String pwd;
+public String getUid() {
+return uid;
+}
+public void setUid(String uid) {
+this.uid = uid;
+}
+public String getPwd() {
+return pwd;
+}
+public void setPwd(String pwd) {
+this.pwd = pwd;
+}
+
+}
---
+++ Bug tracking system/khyathi_b-bug-tracking-system-bd94cfe26f5b/pojo-LoginBean.java
@@ -0,0 +1,160 @@
+package com.bts.beans;
+
+import java.sql.Date;
+
+public class LoginBean {
+	
+	private String Uid;
+	private String Pwd;
+	private int Lid;
+	
+	public String getUid() {
+		return Uid;
+	}
+	public void setUid(String Uid) {
+		this.Uid = Uid;
+	}
+	public String getPwd() {
+		return Pwd;
+	}
+	public void setPwd(String Pwd) {
+		this.Pwd = Pwd;
+	}
+	public int getLid() {
+		return Lid;
+	}
+	public void setLid(int lid) {
+		this.Lid=lid;
+	}
+
+	private String PN;
+	private String PI;
+	private Date  SD;
+	private Date ED;
+	private int NE;
+	private String AM;
+	
+	
+public String getPN() {
+		return PN;
+	}
+	public void setPN(String pN) {
+		this.PN = pN;
+	}
+
+public Date getSD() {
+		return SD;
+	}
+	public void setSD(Date sD) {
+		this.SD = sD;
+	}
+
+public Date getED() {
+		return ED;
+	}
+	public void setED(Date eD) {
+		this.ED = eD;
+	}
+
+public String getAM() {
+		return AM;
+	}
+	public void setAM(String aM) {
+		this.AM = aM;
+	}
+
+	private String Name;
+	private int Phn;
+	private String EM;
+
+
+public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+
+public int getPhn() {
+		return Phn;
+	}
+	public void setPhn(int phn) {
+		Phn = phn;
+	}
+
+public String getEM() {
+		return EM;
+	}
+	public void setEM(String eM) {
+		EM = eM;
+	}
+	public String getPI() {
+		return PI;
+	}
+	public void setPI(String pI) {
+		PI = pI;
+	}
+	public int getNE() {
+		return NE;
+	}
+	public void setNE(int nE) {
+		NE = nE;
+	}
+
+private String post;
+private String pname;
+private String st;
+	
+	
+public String getPost() {
+		return post;
+	}
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getSt() {
+		return st;
+	}
+	public void setSt(String st) {
+		this.st = st;
+	}
+
+	
+	private String BN;
+	private String psn;
+	private String soln;
+		
+			
+	
+public String getBN() {
+		return BN;
+	}
+	public void setBN(String bN) {
+		BN = bN;
+	}
+
+public String getPsn() {
+		return psn;
+	}
+	public void setPsn(String psn) {
+		this.psn = psn;
+	}
+
+public String getSoln() {
+		return soln;
+	}
+	public void setSoln(String soln) {
+		this.soln = soln;
+	}
+
+
+	
+
+}
